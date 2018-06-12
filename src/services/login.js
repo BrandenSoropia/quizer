2a94146efdd07bf5d8eaba85fa12e952ecf72850
Branden Soropia <b.soropia@gmail.com>
2018-06-11 23:38:02 -0400

    testing proxy

--- src/services/login.js
+++ src/services/login.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_API_URL;
-
 export default function login(login_key) {
-  return axios.post(`${baseUrl}/users/login`, { login_key }).then(response => {
+  return axios.post('/api/users/login', { login_key }).then(response => {
     if (response.data.length === 0) {
       alert('invalid username');
     } else {
--- src/services/request.js
+++ src/services/request.js
@@ -1,17 +1,15 @@
 import 'whatwg-fetch';
 
-const baseURL = process.env.REACT_APP_API_URL;
-
 const request = {
   // Return promise passing response value
   get: endpoint => {
-    return fetch(baseURL + endpoint)
+    return fetch('/api' + endpoint)
       .then(response => response.json())
       .catch(err => err);
   },
 
   post: (endpoint, params) => {
-    return fetch(baseURL + endpoint, {
+    return fetch('/api' + endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
