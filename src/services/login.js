b6aa07229605e263974a3bb60c58453be87ada5a
Branden Soropia <b.soropia@gmail.com>
2018-06-17 13:57:33 -0400

    Read api url from env var and use it when sending API requests.

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 export default function login(login_key) {
-  return axios.post('/api/users/login', { login_key }).then(response => {
+  return axios.post(`${BASE_URL}/users/login`, { login_key }).then(response => {
     if (response.data.length === 0) {
       alert('invalid username');
     } else {
--- src/services/request.js
+++ src/services/request.js
@@ -1,15 +1,17 @@
 import 'whatwg-fetch';
 
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 const request = {
   // Return promise passing response value
   get: endpoint => {
-    return fetch('/api' + endpoint)
+    return fetch(BASE_URL + endpoint)
       .then(response => response.json())
       .catch(err => err);
   },
 
   post: (endpoint, params) => {
-    return fetch('/api' + endpoint, {
+    return fetch(BASE_URL + endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
